1cc2a82e09c28c6772199f6b90a5e0bbaad5dc4d
danielhensem <149039948+danielhensem@users.noreply.github.com>
2026-01-16 04:28:42 +0800

    First Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Home } from './home/home';
+import { Services } from './services/services';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'services', component: Services },
+  { path: 'contact', component: Contact },
+];
+
